fdb53e6a3fd1998596ec09b0027df72cd8b6e144
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-22 21:49:21 +0700

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -36,7 +36,7 @@ wa.me/${owner[0]}
   products:[{productId:'7487741964584083'}]}],
   headerImage: { productId: '7487741964584083',
   jpegThumbnail: baper },
-  businessOwnerJid: `6281387089252@s.whatsapp.net`
+  businessOwnerJid: `6289654360447@s.whatsapp.net`
   },
   footerText: 'https://194.233.66.232/',
   }},{})
