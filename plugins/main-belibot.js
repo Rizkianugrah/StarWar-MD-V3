2553f5e3d3352e2d1b3e8e2d56b63b10c3b381a6
Rizkianugrah <46855274+Rizkianugrah@users.noreply.github.com>
2022-07-22 21:44:47 +0700

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,26 +1,56 @@
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-const ftroli = {
-    key : {
-    remoteJid: '6281387089252-1614953337@g.us',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2022,
-    status: 1,
-    surface : 1,
-    message: 'Get Bot', 
-    orderTitle: `NisaBot`,
-    thumbnail: 'https://telegra.ph/file/94d716b0d3cd2858383bc.jpg', 
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
-	conn.reply(m.chat, price1, ftroli)
-}
+  let pepe = 'https://telegra.ph/file/2e2eb7d093fec08fd00d4.jpg'
+  let baper = await fetch(pepe).then(a => a.buffer())
 
-handler.help = ['sewa', 'sewabot']
-handler.tags = ['info']
-handler.command = /^(sewa|sewabot)?$/i
+  let aine = '6281387089252@s.whatsapp.net'
+  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/94d716b0d3cd2858383bc.jpg")
+  let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/94d716b0d3cd2858383bc.jpg")
+  let c = pickRandom([a, b])
+  let d = await fetch(c).then(a => a.buffer())
+  let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
+  title: `${await conn.getName(conn.user.jid)}`,
+  description: ` *• SEWA BOT & UP TO PREMIUM •*
+        
+1. Grup / 30 Hari
+Rp. 15.000 Dana
+Rp. 15.000 Pulsa
+
+2. Premium / 30 Hari
+Rp. 10.000 Dana
+Rp. 10.000 Pulsa
+
+3. Premium + Grup / 30 Hari
+Rp. 20.000 Dana
+Rp. 20.000 Pulsa
+
+wa.me/${owner[0]}
+*Bukan Bot!!!*
+*Owner ${conn.user.name}*
+`,
+  buttonText: 'Harga Sesuai Pasaran',
+  listType: 2,
+  productListInfo: {
+  productSections: [{
+  title:'Klik untuk order',
+  products:[{productId:'7487741964584083'}]}],
+  headerImage: { productId: '7487741964584083',
+  jpegThumbnail: baper },
+  businessOwnerJid: `6281387089252@s.whatsapp.net`
+  },
+  footerText: 'https://194.233.66.232/',
+  }},{})
+  conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
+}
+handler.help = ['sewa']
+handler.tags = ['main']
+handler.command = /^(sewa)$/i
 
 module.exports = handler
+
+function pickRandom(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
